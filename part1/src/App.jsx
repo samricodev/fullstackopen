e58ae2c780b62fb15b7681efe0fbb4573f86738b
samricodev <samuel.isaac.rico.estrada@gmail.com>
2023-07-07 10:23:45 -0600

    anectodes exercises

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,53 +1,42 @@
-import Boton from "./components/Boton"
-import Statistics from "./components/Statistics"
 import { useState } from "react"
+import Boton from './components/Boton'
 
 function App() {
-  const [ feedback, setFeedBack] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0
-  })
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState({0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0})
 
-  const good = () => {
-    const newFeedBack = {
-      ...feedback,
-      good: feedback.good + 1
-      
-    }
-    setFeedBack(newFeedBack)
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+
+  const next = () => {
+    const random = Math.floor(Math.random() * anecdotes.length)
+    setSelected(random)
   }
 
-  const neutral = () => {
-    const newFeedBack = {
-      ...feedback,
-      neutral: feedback.neutral + 1,
-    }
-    setFeedBack(newFeedBack)
+  const newVotes = () => {
+    const copy = {...votes}
+    copy[selected] += 1
+    setVotes(copy)
   }
-
-  const bad = () => {
-    const newFeedBack = {
-      ...feedback,
-      bad: feedback.bad +1
-    }
-    setFeedBack(newFeedBack)
-  }
-
-  const totalFeedBack = (feedback.good + feedback.neutral + feedback.bad) 
-
-  const promedio = ((feedback.good * 1) + (feedback.neutral * 0 ) + (feedback.bad * -1)) / totalFeedBack
-
-  const positivos = (feedback.good / totalFeedBack) * 100
 
   return (
     <div>
-      <h2>Give FeedBack</h2>
-      <Boton handleClick={good} text="good"/>
-      <Boton handleClick={neutral} text="neutral"/>
-      <Boton handleClick={bad} text="bad"/>
-      <Statistics  feedback={feedback} positivos={positivos} promedio={promedio} totalFeedBack={totalFeedBack}/>
-    </div>
+      <h2>Anecdote of the day</h2>
+      {anecdotes[selected]}
+      <br />
+      has {votes[selected]} votes
+      <br />
+      <Boton handleClick={newVotes} text="votes" />
+      <Boton handleClick={next} text="next anecdotes" />
+      <h2>Anecdote with most votes</h2>
+      {anecdotes[Object.keys(votes).reduce((a, b) => votes[a] > votes[b] ? a : b)]}
+    </div> 
   )
 }
 
